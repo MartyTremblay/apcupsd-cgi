33fa7ac4af2a501b45284f21b509138d56f0a6f3
Martin Tremblay <MartyTremblay@users.noreply.github.com>
2017-06-10 21:42:55 -0400

    Now forcing init script to believe that it has been configured

--- startup.sh
+++ startup.sh
@@ -4,5 +4,6 @@ sed -i "s/UPSCABLE smart/UPSCABLE usb/g" /etc/apcupsd/apcupsd.conf
 sed -i "s/NISIP 127.0.0.1/NISIP 0.0.0.0/g" /etc/apcupsd/apcupsd.conf
 sed -i "s/UPSTYPE apcsmart/UPSTYPE usb/g" /etc/apcupsd/apcupsd.conf
 sed -i "s/DEVICE \/dev\/ttyS0/DEVICE/g" /etc/apcupsd/apcupsd.conf
+sed -i "s/ISCONFIGURED=no/ISCONFIGURED=yes/g" /etc/default/apcupsd
 
 apcupsd -b;
